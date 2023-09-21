3bbbc8227cfc76dda4ce26a2e3b25a03b7af9029
hansc <hanscabanero@gmail.com>
2023-09-21 16:29:31 -0600

    added draw, eraser, color features and css

--- script.js
+++ script.js
@@ -1,10 +1,56 @@
-const container = document.getElementById('container');
+let color = 'black';
+let click = true;
 
-function makeDivs(numDivs) {
-for (let d = 0; d < numDivs; d++){
-    let cells = document.createElement('div');
-    container.appendChild(cells)    
-    }
-    }
+function populateBoard(size) {
+    let board = document.querySelector(".board")
+    let squares = board.querySelectorAll('div')
+    squares.forEach((div) => div.remove());
+board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-makeDivs(256);
+let amount = size * size;
+    for(let i = 0; i < amount; i ++)
+    {
+        let square = document.createElement("div");
+        square.addEventListener("mouseover", colorSquare);
+        square.style.backgroundColor = "white";
+        board.insertAdjacentElement("beforeend", square);
+    
+    }
+}
+populateBoard(16);
+function changeSize(input) {
+    if(input >-2 || input <= 100){
+            populateBoard(input);
+    } else {
+        console.log("only accepting values between 2 and 100");
+    }
+}
+function colorSquare() {
+    if(click) {
+      if(color === "random")
+        {
+            this.style.backgroundColor = `hsl(${Math.random()*360}, 100%, 50%)`;
+        } else {
+            this.style.backgroundColor = color;
+        }  
+    }
+}
+
+function changeColor(choice) {
+    color = choice;
+}
+
+function resetBoard(){
+    let board = document.querySelector(".board")
+    let squares = board.querySelectorAll('div')
+    squares.forEach((div) => div.style.backgroundColor= 'white');
+}
+document.querySelector("body").addEventListener("click", () =>{
+    click = !click;
+    if(click) {
+        document.querySelector('.mode').textContent = "Mode: Coloring"
+    } else {
+        document.querySelector('.mode').textContent = "Mode: Not Coloring"
+    }
+});
